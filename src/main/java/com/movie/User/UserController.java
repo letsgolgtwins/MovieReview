de2471957964083a6cc9fa831f5186149f3b4760
LeeMinHee <98minh2@gmail.com>
2024-08-01 12:38:15 +0900

    240801 로그인/회원가입 및 메인 페이지 fragment

---
+++ src/main/java/com/movie/User/UserController.java
@@ -0,0 +1,19 @@
+package com.movie.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+	// 회원가입 페이지로 이동
+	// http://localhost/user/sign-up-view
+	@GetMapping("/sign-up-view")
+	public String SignUpView() {
+		return "user/SignUp";
+	}
+	
+	
+}
